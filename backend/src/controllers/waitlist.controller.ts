cd545ae4b36f0f78ea7186dac9bdfb2960a8543e
mrskg0438 <gaudshwetkamal0438@gmail.com>
2025-06-04 00:11:18 +0530

    validation changes

--- backend/src/controllers/waitlist.controller.ts
+++ backend/src/controllers/waitlist.controller.ts
@@ -4,16 +4,23 @@ import { Client } from '../models/waitlist.model';
 export const submitClientOnboarding = async (req: Request, res: Response): Promise<void> => {
     try {
         const data = req.body;
-
-        if (!data.email || !data.industry || !data.size) {
-            res.status(400).json({ message: 'Missing required fields.' });
-            return;
+        if (data.role = 'Client') {
+            if (!data.email || !data.industry || !data.size) {
+                res.status(400).json({ message: 'Missing required fields.' });
+                return;
+            }
+        }
+        else{
+            if (!data.email || !data.industry) {
+                res.status(400).json({ message: 'Missing required fields.' });
+                return;
+            }
         }
 
         const newClient = new Client(data);
         await newClient.save();
         res.status(201).json({ message: `${data.role} onboarded successfully.` });
-     
+
     } catch (err) {
         console.error('Onboarding error:', err);
         res.status(500).json({ message: `Server error while onboarding ${req.body.data.role}.` })
